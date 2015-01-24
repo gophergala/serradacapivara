ed700368ced03094506fde3005959e125a2fd521
claudemiro <dimiro1@gmail.com>
2015-01-23 22:56:49 -0300

    Added SQLX Annotations

--- entities/entities.go
+++ entities/entities.go
@@ -1,36 +1,36 @@
 package entities
 
 type Site struct {
-	Id              int
-	Name            string
-	HasPicture      bool
-	HasEngraving    bool
-	Other           bool
-	IsHistoric      bool
-	YearOfDiscovery int
+	Id              int    `db:"id"`
+	Name            string `db:"name"`
+	HasPicture      bool   `db:"has_picture"`
+	HasEngraving    bool   `db:"has_engraving"`
+	Other           bool   `db:"other"`
+	IsHistoric      bool   `db:"is_historic"`
+	YearOfDiscovery int    `db:"year_of_discovery"`
 
-	CityId         int
-	CircuitId      int
-	NationalParkId int
-	LocationId     int
+	CityId         int `db:"city_id"`
+	CircuitId      int `db:"circuit_id"`
+	NationalParkId int `db:"national_park_id"`
+	LocationId     int `db:"location_id"`
 }
 
 type City struct {
-	Id   int
-	Name string
+	Id   int    `db:"id"`
+	Name string `db:"name"`
 }
 
 type Circuit struct {
-	Id   int
-	Name string
+	Id   int    `db:"id"`
+	Name string `db:"name"`
 }
 
 type NationalPack struct {
-	Id   int
-	Name string
+	Id   int    `db:"id"`
+	Name string `db:"name"`
 }
 
 type Location struct {
-	Id   int
-	Name string
+	Id   int    `db:"id"`
+	Name string `db:"name"`
 }
