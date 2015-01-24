6c8a5d78460bdaeaa34370b37297625e9ff2c3f3
Dann Luciano <dannluciano@gmail.com>
2015-01-24 01:30:26 -0300

    ADDED column is_graving in Sites table

--- db/schema.sql
+++ db/schema.sql
@@ -23,14 +23,18 @@ CREATE TABLE IF NOT EXISTS sites (
   id                SERIAL PRIMARY KEY,
   name              varchar,
   has_picture       boolean,
+  has_engraving     boolean,
   other             boolean,
   is_historic       boolean,
   year_of_discovery integer,
 
-  city_id          integer REFERENCES cities(id),
-  circuit_id       integer REFERENCES circuits(id),
-  national_park_id integer REFERENCES national_parks(id),
-  location_id      integer REFERENCES locations(id)
+  latitude          varchar,
+  longitude         varchar,
+
+  city_id           integer REFERENCES cities(id),
+  circuit_id        integer REFERENCES circuits(id),
+  national_park_id  integer REFERENCES national_parks(id),
+  location_id       integer REFERENCES locations(id)
 );
 
 CREATE EXTENSION unaccent;
