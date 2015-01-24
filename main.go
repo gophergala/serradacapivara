c14248aaa21b13d9186693a32eb021349b0de50f
claudemiro <dimiro1@gmail.com>
2015-01-24 16:41:28 -0300

    Fixed error in compilation

--- main.go
+++ main.go
@@ -15,10 +15,6 @@ func main() {
 	goji.Get("/site/:id", handlers.Site)
 	goji.Get("/about", handlers.About)
 
-	// Admin
-	goji.Get("/admin/newsite", handlers.NewSite)
-	goji.Post("/admin/newsite", handlers.RegisterSite)
-
 	// Static
 	goji.Get("/static/*", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	goji.Get("/layout/*", http.StripPrefix("/layout/", http.FileServer(http.Dir("layout"))))
